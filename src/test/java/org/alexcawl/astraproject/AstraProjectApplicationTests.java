374f83dd8eeccc108f43c47b0b81af916be09390
AlexCawl <babushkinsusu@gmail.com>
2022-09-18 21:43:53 +0500

    Added tests

---
+++ src/test/java/org/alexcawl/astraproject/AstraProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alexcawl.astraproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstraProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
